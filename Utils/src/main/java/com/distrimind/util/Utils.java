ad58d495947492fbcc1459a25b430189ddd79971
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2021-02-19 16:19:52 +0100

    Update date

--- Utils/src/main/java/com/distrimind/util/Utils.java
+++ Utils/src/main/java/com/distrimind/util/Utils.java
@@ -67,7 +67,7 @@ public class Utils {
 			VERSION.addCreator(new Person("mahdjoub", "jason"))
 					.addDeveloper(new PersonDeveloper("mahdjoub", "jason", "2016-01-04"))
 					.addDescription(
-							new Description((short)5, (short)17, (short)0, Version.Type.STABLE, (short)0, "2020-02-18")
+							new Description((short)5, (short)17, (short)0, Version.Type.STABLE, (short)0, "2020-02-19")
 									.addItem("Add functions into WrappedData and WrappedString")
 									.addItem("Fix bad parameter into function ServerASymmetricEncryptionAlgorithm.decode(byte[], int, int)")
 									.addItem("Fix security issue into KeyWrapperAlgorithm class : signatures where not always generated")
